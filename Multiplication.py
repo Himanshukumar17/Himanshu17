d09e997ebebca70a628a6ae2fed54f8b9e87207a
Himanshukumar17 <kumar.himanshu526@gmail.com>
2018-02-02 10:48:03 +0530

    2 feb

--- Multiplication.py
+++ Multiplication.py
@@ -1,9 +1,3 @@
-while True:
-	print("Enter 'x' for exit.")
-	num = input("Enter a number: ")
-	if num == 'x':
-		break
-	else:
-		number = int(num)
-		for i in range(1, 11):
-			print(number, "*", i, "=", number*i)
+n=int(input('enter number'))
+for i in range(1,11):
+    print(str(n)+' * '+str(i)+' = '+str(n*i))
