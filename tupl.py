958aec302898ba07c80af78c141eddca7b7955d5
Himanshukumar17 <kumar.himanshu526@gmail.com>
2018-02-05 10:08:48 +0530

    tuple

---
+++ tupl.py
@@ -0,0 +1,19 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> tupl=11,22,33,44,55
+>>> tupl
+(11, 22, 33, 44, 55)
+>>> x=10
+>>> y=20
+SyntaxError: multiple statements found while compiling a single statement
+>>> x=10
+>>> y=20
+>>> tup2=x,y
+>>> tup2
+(10, 20)
+>>> a,b=tup2
+>>> a
+10
+>>> b
+20
+>>> 
